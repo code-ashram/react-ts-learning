4dbab2bcf99c65f14b3f5dd5e52a88a8ef81dd54
Andrei Yeutukhou <gavnorock84@hotmail.com>
2023-07-08 20:46:21 +0300

    reamke first part og task-3

--- src/components/Costs/Costs.tsx
+++ src/components/Costs/Costs.tsx
@@ -2,37 +2,34 @@ import {FC, useState} from "react";
 import CostItem from "./CostItem/CostItem.jsx";
 import {Item} from "../../data.tsx";
 import Card from "../Card/Card.tsx";
-
-import "./Costs.css"
 import CostsFilter from "./CostFilter/CostsFilter.tsx";
 import {currentYear} from "../../utils.ts";
+
+import "./Costs.css"
 
 type Props = {
     source: Item[]
 }
 
-const getCurrentYearCosts = (array: Item[], year: number) => array.filter((item) => item.date.getFullYear() === year)
-
 const Costs: FC<Props> = ({source}) => {
-    const [selectedYear, setSelectedYear] = useState<number>(currentYear)
-    const [costsList, setCostsList] = useState<Item[]>(getCurrentYearCosts(source, currentYear))
+    const [selectedYear, setSelectedYear] = useState(currentYear)
+
+    const handleChangeYear = (year: number) => {
+        setSelectedYear(year)
+    }
+
+    const filteredList = source.filter((item) => item.date.getFullYear() === selectedYear)
+
     const showCostsList = (array: Item[]) => array.map((item) => <CostItem
         key={item.id}
         date={item.date}
         description={item.description}
         amount={item.amount}/>)
 
-    const handleChangeYear = (year: number) => {
-        setSelectedYear(year)
-        setCostsList(
-            source.filter((item) =>
-            item.date.getFullYear() === year))
-    }
-
     return (
         <Card className="costs">
             <CostsFilter year={selectedYear} onChangeYear={handleChangeYear}/>
-            {showCostsList(selectedYear ? costsList : source)}
+            {showCostsList(selectedYear ? filteredList : source)}
         </Card>
     )
 }
