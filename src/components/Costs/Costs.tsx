75ec16bc93a9b2348b0314a41d19bf4da115ae91
Andrei Yeutukhou <gavnorock84@hotmail.com>
2023-07-11 21:31:24 +0300

    fix something wrong in the form

--- src/components/Costs/Costs.tsx
+++ src/components/Costs/Costs.tsx
@@ -1,4 +1,4 @@
-import {FC, useState} from "react";
+import {FC, useEffect, useState} from "react";
 import CostItem from "./CostItem/CostItem.jsx";
 import {Item} from "../../data.tsx";
 import Card from "../Card/Card.tsx";
@@ -11,27 +11,37 @@ type Props = {
     source: Item[]
 }
 
+const getCurrentYearCosts = (array: Item[], year: number) => array.filter((item) => item.date.getFullYear() === year)
+
 const Costs: FC<Props> = ({source}) => {
-    const [selectedYear, setSelectedYear] = useState<string>(currentYear)
+    const [selectedYear, setSelectedYear] = useState<number | undefined>(currentYear)
+    const [costsList, setCostsList] = useState<Item[]>(getCurrentYearCosts(source, currentYear))
+    useEffect(() => {
+        console.table(costsList)
+    }, [costsList])
 
-    const filteredList = source.filter((item) => item.date.getFullYear().toString() === selectedYear)
-
-    const handleChangeYear = (year: string) => {
+    const handleChangeYear = (year: number | undefined):void => {
         setSelectedYear(year)
+        setCostsList(year ? source.filter((item) => item.date.getFullYear() === year) : source)
     }
 
-    const showCostsList = (array: Item[]) => array.map((item) => <CostItem
-        key={item.id}
-        date={item.date}
-        description={item.description}
-        amount={item.amount}/>)
+    // const filteredList = source.filter((item) => item.date.getFullYear() === selectedYear)
 
     return (
         <Card className="costs">
             <CostsFilter year={selectedYear} onChangeYear={handleChangeYear}/>
-            {showCostsList(selectedYear === "Все расходы" ? source : filteredList)}
+            {costsList.length
+                ? costsList.map((item) => <CostItem
+                    key={item.id}
+                    date={item.date}
+                    description={item.description}
+                    amount={item.amount}/>)
+                : <p>Нет расходов</p>
+            }
         </Card>
     )
 }
 
 export default Costs
+
+
--- src/utils.ts
+++ src/utils.ts
@@ -1,2 +1,2 @@
 export const today: string = (new Date()).toISOString().slice(0, 10)
-export const currentYear: string = new Date().getFullYear().toString()
+export const currentYear: number = new Date().getFullYear()
